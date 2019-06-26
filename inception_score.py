cc0bf40c8ad923e2af17c351e457ad8a0d5fd1e4
hashiting <leepengqiu@gmail.com>
2019-06-26 22:37:22 +0200

    Add files via upload

---
+++ inception_score.py
@@ -0,0 +1,111 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as F
+import torch.utils.data
+
+from torchvision.models.inception import inception_v3
+
+import numpy as np
+from scipy.stats import entropy
+def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
+    """Computes the inception score of the generated images imgs
+
+    imgs -- Torch dataset of (3xHxW) numpy images normalized in the range [-1, 1]
+    cuda -- whether or not to run on GPU
+    batch_size -- batch size for feeding into Inception v3
+    splits -- number of splits
+    """
+    N = len(imgs)
+    print(N)
+    print(imgs[0].shape)
+    assert batch_size > 0
+    assert N > batch_size
+
+    # Set up dtype
+    if cuda:
+        dtype = torch.cuda.FloatTensor
+    else:
+        if torch.cuda.is_available():
+            print("WARNING: You have a CUDA device, so you should probably set cuda=True")
+        dtype = torch.FloatTensor
+
+    # Set up dataloader
+    dataloader = torch.utils.data.DataLoader(imgs, batch_size=batch_size)
+
+    # Load inception model
+    inception_model = inception_v3(pretrained=True, transform_input=False).type(dtype)
+    inception_model.eval();
+    up = nn.Upsample(size=(299, 299), mode='bilinear').type(dtype)
+    def get_pred(x):
+        if resize:
+            x = up(x)
+        x = inception_model(x)
+        return F.softmax(x).data.cpu().numpy()
+
+    # Get predictions
+    preds = np.zeros((N, 1000))
+
+    for i, batch in enumerate(dataloader, 0):
+        batch = batch.type(dtype)
+        batchv = Variable(batch)
+        batch_size_i = batch.size()[0]
+
+        preds[i*batch_size:i*batch_size + batch_size_i] = get_pred(batchv)
+
+    # Now compute the mean kl-div
+    split_scores = []
+
+    for k in range(splits):
+        part = preds[k * (N // splits): (k+1) * (N // splits), :]
+        py = np.mean(part, axis=0)
+        scores = []
+        for i in range(part.shape[0]):
+            pyx = part[i, :]
+            scores.append(entropy(pyx, py))
+        split_scores.append(np.exp(np.mean(scores)))
+
+    return np.mean(split_scores), np.std(split_scores)
+
+if __name__ == '__main__':
+    class IgnoreLabelDataset(torch.utils.data.Dataset):
+        def __init__(self, orig):
+            self.orig = orig
+
+        def __getitem__(self, index):
+            return self.orig[index][0]
+
+        def __len__(self):
+            return len(self.orig)
+
+    import torchvision.datasets as dset
+    import torchvision.transforms as transforms
+    from torch.utils.data.dataset import Dataset
+    from PIL import Image
+    import os
+    Image_path = os.listdir('valid/single/')
+    transform = transforms.Compose([
+                                     #transforms.Scale(64),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                     ])
+    def default_loader(path):
+        img_pil =  Image.open(path)
+        #img_pil = Image.fromarray(np.array(img_pil).transpose(2,0,1))
+        img_tensor = transform(img_pil)
+        return img_tensor   
+    class MyDataset(Dataset):
+        def __init__(self,loader=default_loader):
+            # stuff
+            self.images = Image_path
+            self.loader = loader
+            
+        def __getitem__(self, index):
+            # stuff
+            image_path = 'valid/single/'+self.images[index]
+            return self.loader(image_path)
+    
+        def __len__(self):
+            return len(self.images)
+    print ("Calculating Inception Score...")
+    print (inception_score(MyDataset(), cuda=True, batch_size=64, resize=True, splits=10))
